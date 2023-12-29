f9161b050bb2acdae684b385552b7352578eb4a8
ibnaleem <ibnaleem@outlook.com>
2023-12-29 22:35:23 +0000

    fixes

--- src/cogs/encryption.py
+++ src/cogs/encryption.py
@@ -2,57 +2,62 @@ import discord, gnupg
 from discord.ext import commands
 from discord.ui import Modal, TextInput
 from discord import app_commands, Interaction
+from concurrent.futures import ThreadPoolExecutor
 
 
 class EncryptionModal(Modal, title="Encrypt Text Using Key Fingerprint"):
-    
     fingerprint = TextInput(
         style=discord.TextStyle.short,
-        label="Key Fingerprint",
+        label="Public Key",
         required=True,
-        placeholder="example: 2024EC023F2769E66181C0F581B4A2A862BBADE"
+        placeholder="example: 20247EC023F2769E66181C0F581B4A2A862BBADE",
     )
 
     message = TextInput(
         style=discord.TextStyle.long,
         label="Message",
         required=True,
-        placeholder="Your message"
+        placeholder="Your message",
     )
 
     async def on_submit(self, interaction: Interaction):
-        encrypted_text = self.encrypt_text(message=self.message.value, recipient_fingerprint=self.fingerprint.value)
+        await interaction.response.defer(ephemeral=True, thinking=True)
+
+        fingerprint = self.fingerprint.value
+        message = self.message.value
+
+        encrypted_text = encrypt_text(message=message, fingerprint=fingerprint)
 
         try:
+            print(encrypted_text)
             await interaction.user.send(encrypted_text)
-            await interaction.response.send_message(f"{interaction.user.mention} Check your DMs", ephemeral=True)
+            await interaction.followup.send(f"{interaction.user.mention} Check your DMs", ephemeral=True)
         except discord.Forbidden:
-            await interaction.response.send_message(encrypted_text)
+            msg = await interaction.followup.send("*Encrypting your text...*")
+            await msg.edit(content=encrypted_text)
+        except discord.HTTPException as e:
+            print(e)
 
-    def encrypt_text(self, message: str, recipient_fingerprint: str) -> str:
-        
-        gpg = gnupg.GPG()
-        
-        encrypted_data = gpg.encrypt(message, recipients=[recipient_fingerprint])
-
-        if encrypted_data.ok:
-            return str(encrypted_data)
-        else:
-            raise ValueError(f"Encryption failed: {encrypted_data.status}")
+def encrypt_text(message: str, fingerprint: str) -> str:
+    gpg = gnupg.GPG()
+    result = gpg.encrypt(message, recipients=[fingerprint])
+    if result.ok:
+        print(str(result))
+        return str(result)
+    else:
+        print(f"Encryption failed: {result.status}")
+        return "Encryption failed."
 
 class Encryption(commands.Cog):
     def __init__(self, client: commands.Bot) -> None:
         super().__init__()
         self.client = client
 
-    @app_commands.command(name="encrypt", description="Encrypt text using key fingerprint")
-    @app_commands.describe(fingerprint="Recipient's fingerprint used for encryption")
-    async def encrypt(self, interaction: Interaction, fingerprint: str, message: str):
-
+    @app_commands.command(name="encrypt", description="Encrypt text using public key")
+    @app_commands.describe()
+    async def encrypt(self, interaction: Interaction):
         encryption_modal = EncryptionModal()
-
         await interaction.response.send_modal(encryption_modal)
-
 
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Encryption(client))
