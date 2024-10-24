2011ba6ebf496428af420e193631e75d3002fbf1
ibnaleem <ibnaleem@outlook.com>
2024-10-24 16:54:12 +0100

    create help command

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ export const execute = async (interaction) => {
   const embed = new EmbedBuilder()
     .setTitle('/help')
     .setDescription("*Disclaimer: Encryption takes place via Discord's API. While I am [open-sourced](https://github.com/ibnaleem/gnupg-discord), please refrain from sending any sensitive information through Discord. When you submit a message, it is transmitted through Discord's API to me, where I encrypt it and then send it to the recipient via the same API. For sensitive data, it's best to run GnuPG on your own device. My goal is to help mobile users or those unfamiliar with the GPG CLI to securely encrypt messages to others, particularly when they don't know the recipient's key.*")
-    .setColor("#00008B")
+    .setColor("#3498db")
     .addFields(
       { name: '/encrypt', value: "Send an encrypted message to a user. [I do not store this.](https://github.com/ibnaleem/gnupg-discord/blob/main/commands/encrypt.js#L64-L128)" },
       { name: '/set-gpg-key', value: 'Set your GPG key so other users can encrypt messages to you.' },
